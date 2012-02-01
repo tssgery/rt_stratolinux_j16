907c86aaa94b833afb0d7315b358828fb295022c
Eric Young <eric@aceshome.com>
2012-01-31 20:55:41 -0500

    initial checkin

---
+++ template-overview.php
@@ -0,0 +1,10 @@
+<h1>RocketTheme Afterburner</h1>
+
+<img src="../templates/rt_stratolinux_j16/template_thumbnail.png" style="border: 4px solid #ccc; padding: 2px; float: left; margin-bottom: 10px;" /><div class="clr"></div><br />Afterburner is a lightning fast GPL template designed specifically for speed and SEO.
+
+<h3>Changelog</h3>
+
+<b>1.6.0</b><br />
+<ul>
+  <li>Initial Release</li>
+</ul>
